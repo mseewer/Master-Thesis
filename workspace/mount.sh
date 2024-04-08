b8add8b933317077e360c6fdc5bfc7c38a6ca92b
mseewer <4114029-mseewer@users.noreply.gitlab.com>
2024-04-08 11:30:56 +0200

    add + update helper scripts

---
+++ workspace/mount.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Mount kali remote directory to local directory "remote" using sshfs
+
+sshfs kaliCYD:/home/lab/Documents remote
--- workspace/sync.sh
+++ workspace/sync.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
-# Sync local directory to kali remote directory which is mounted using sshfs on dir "remote"
+# Sync local directory (usually containing code) to kali remote directory
+# which is mounted using sshfs on dir "remote" (execute mount.sh first)
+# Enables having code in this repo and running it on kali (without copying, push/pull, etc.)
 
 LOCAL_DIR=$1
 # SSHFS_DIR=$2
