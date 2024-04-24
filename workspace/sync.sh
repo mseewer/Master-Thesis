07fca76a1b3d7855298b61b61575749493f13f62
mseewer <4114029-mseewer@users.noreply.gitlab.com>
2024-04-24 09:06:57 +0200

    add helper scripts for syncing files from local to VM

--- workspace/sync.sh
+++ workspace/sync.sh
@@ -8,6 +8,7 @@ LOCAL_DIR=$1
 # SSHFS_DIR=$2
 # REMOTE_HOST="user@remote_host"
 
-while inotifywait -r -e modify,create,delete,move "$LOCAL_DIR"; do
-    rsync -avz --delete "$LOCAL_DIR" "remote/$LOCAL_DIR"
+# exclude output directory from sync (and deleting on remote)
+while inotifywait -r -e modify,create,delete,move --exclude "$LOCAL_DIR/output" "$LOCAL_DIR"; do
+    rsync -avz --exclude="$LOCAL_DIR/output" "$LOCAL_DIR" "remote/$LOCAL_DIR"
 done
