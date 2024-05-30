77e4b5d3cd24c247e2846e6044f3f9bae7743bd1
mseewer <4114029-mseewer@users.noreply.gitlab.com>
2024-05-30 10:54:55 +0200

    modify mount helper script to mount all kali VMs

--- workspace/mount.sh
+++ workspace/mount.sh
@@ -2,4 +2,21 @@
 
 # Mount kali remote directory to local directory "remote" using sshfs
 
-sshfs kaliCYD:/home/lab/Documents remote
+loc=$1
+# check if loc is one of Zurich, Thun, or Lausanne and set SSH_target variable
+if [[ $loc == "Zurich" ]]; then
+    SSH_target=kaliZH
+    USER=lab
+elif [[ $loc == "Thun" ]]; then
+    SSH_target=kaliThun
+    USER=lab
+elif [[ $loc == "Lausanne" ]]; then
+    SSH_target=kaliLAU
+    USER=kali
+else
+    echo "Invalid location. Please provide one of Zurich, Thun, or Lausanne."
+    exit 1
+fi
+
+
+sshfs ${SSH_target}:/home/${USER}/Documents remote
